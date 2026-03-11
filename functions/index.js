8b4a3222493e1ac92132204e53fffd703f111312
teceudesenvolvo <blutecnologiasbr@gmail.com>
2026-03-11 18:37:54 -0300

    firebase-func

--- functions/index.js
+++ functions/index.js
@@ -1,32 +1,43 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+const {onValueCreated} = require("firebase-functions/v2/database");
+const {defineString} = require("firebase-functions/params");
+const admin = require("firebase-admin");
+const nodemailer = require("nodemailer");
 
-const {setGlobalOptions} = require("firebase-functions");
-const {onRequest} = require("firebase-functions/https");
-const logger = require("firebase-functions/logger");
+admin.initializeApp();
 
-// For cost control, you can set the maximum number of containers that can be
-// running at the same time. This helps mitigate the impact of unexpected
-// traffic spikes by instead downgrading performance. This limit is a
-// per-function limit. You can override the limit for each function using the
-// `maxInstances` option in the function's options, e.g.
-// `onRequest({ maxInstances: 5 }, (req, res) => { ... })`.
-// NOTE: setGlobalOptions does not apply to functions using the v1 API. V1
-// functions should each use functions.runWith({ maxInstances: 10 }) instead.
-// In the v1 API, each function can only serve one request per container, so
-// this will be the maximum concurrent request count.
-setGlobalOptions({ maxInstances: 10 });
+const gmailEmail = defineString("GMAIL_EMAIL");
+const gmailPassword = defineString("GMAIL_PASSWORD");
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+let mailTransport;
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.sendMailOnNewRequest = onValueCreated(
+    "/{city}/mail/{pushId}",
+    async (event) => {
+      if (!mailTransport) {
+        mailTransport = nodemailer.createTransport({
+          service: "gmail",
+          auth: {
+            user: gmailEmail.value(),
+            pass: gmailPassword.value(),
+          },
+        });
+      }
+
+      const snapshot = event.data;
+
+      if (!snapshot) return;
+
+      const mailData = snapshot.val();
+
+      const mailOptions = {
+        from: `"Portal de Serviços" <${gmailEmail.value()}>`,
+        to: mailData.to,
+        subject: mailData.message.subject,
+        html: mailData.message.html,
+      };
+
+      await mailTransport.sendMail(mailOptions);
+
+      return snapshot.ref.remove();
+    },
+);
